631522b14cd754a87a927becafc856bfc6328308
Adel <leda91@hotmail.fr>
2021-12-07 00:19:08 +0100

    lanecment projet 5

---
+++ front/javascript/index.js
@@ -0,0 +1,8 @@
+let kanapAll = [];
+
+const fetchKanap = async () => {
+    await fetch("http://localhost:3000/api/products").then((res) => res.json())
+    .then((promise) => console.log(promise));
+};
+
+fetchKanap();
